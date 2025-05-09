54915b427a05eff1775dde9243314a8d088a5341
G4ngst4C0d3 <andersonleite.pl013@academico.ifsul.edu.br>
2025-05-09 15:29:47 -0300

    resources

---
+++ atividades_AndersonLeite/app/Http/Controllers/BibliotecaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Biblioteca;
+use Illuminate\Http\Request;
+
+class BibliotecaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Biblioteca $biblioteca)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Biblioteca $biblioteca)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Biblioteca $biblioteca)
+    {
+        //
+    }
+}
--- atividades_AndersonLeite/routes/web.php
+++ atividades_AndersonLeite/routes/web.php
@@ -15,28 +15,26 @@ Route::get('/', function () {
 //     echo "<h2>Ola Mundo com Laravel</h2>";
 // });
 
-Route::get('/ola', [HomeController::class, 'index']);
-Route::get('/produtos', [App\Http\Controllers\ProdutoController::class, 'index'])->name('produtos');
-Route::get('/produtos/{id}', [ProdutoController::class, 'show']);
-Route::get('/noticias', [NoticiaController::class, 'index']);
-Route::get('/noticias/{id}', [NoticiaController::class, 'show']);
+Route::prefix('/produtos')->group(function () {
+    Route::get('/', [App\Http\Controllers\ProdutoController::class, 'index'])->name('produtos');
+    Route::get('/{id}', [App\Http\Controllers\ProdutoController::class, 'show'])->name('produtos.show');
+    Route::get('/create', [ProdutoController::class, 'create']);
+    Route::post('/', [ProdutoController::class, 'store'])->name('produto.store');
+    Route::get('/{id}/edit', [ProdutoController::class, 'edit'])->name('produto.edit');
+    Route::put('/{id}/update', [ProdutoController::class, 'update'])->name('update');
+    Route::post('/{id}/delete', [ProdutoController::class, 'remove'])->name('remove');
+    Route::delete('/{id}/delete', [ProdutoController::class, 'delete'])->name('delete');
+});
 
-//create
-Route::get('/produto', [ProdutoController::class, 'create']);
-Route::post('/produto', [ProdutoController::class, 'store'])->name('produto.store');
-Route::get('/noticia', [App\Http\Controllers\NoticiaController::class, 'create']);
-Route::post('/noticia', [App\Http\Controllers\NoticiaController::class, 'store'])->name('noticia.store');
-Route::get('/noticia/{id}', [NoticiaController::class, 'show']);
+Route::prefix('/noticias')->group(function () {
+    Route::get('/noticias', [NoticiaController::class, 'index']);
+    Route::get('/noticias/{id}', [NoticiaController::class, 'show']);
+    Route::get('/noticia', [App\Http\Controllers\NoticiaController::class, 'create']);
+    Route::post('/noticia', [App\Http\Controllers\NoticiaController::class, 'store'])->name('noticia.store');
+    Route::get('/noticia/{id}', [NoticiaController::class, 'show']);
+    Route::get('/noticia/{id}/edit', [NoticiaController::class, 'edit'])->name('noticia.edit');
+    Route::put('/noticia/{id}', [NoticiaController::class, 'update'])->name('noticia.update');
+    Route::delete('/noticia/{id}', [NoticiaController::class, 'delete'])->name('noticia.delete');
 
-
-//update
-Route::get('/produto/{id}/edit', [ProdutoController::class, 'edit'])->name('produto.edit');
-Route::put('/produto/{id}/update', [ProdutoController::class, 'update'])->name('update');
-Route::get('/noticia/{id}/edit', [NoticiaController::class, 'edit'])->name('noticia.edit');
-Route::put('/noticia/{id}', [NoticiaController::class, 'update'])->name('noticia.update');
-
-//delete
-Route::delete('/produto/{id}/delete', [ProdutoController::class, 'delete'])->name('delete');
-Route::post('/produto/{id}/delete', [ProdutoController::class, 'remove'])->name('remove');
-Route::delete('/noticia/{id}', [NoticiaController::class, 'delete'])->name('noticia.delete');
+});
 
